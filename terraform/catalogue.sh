c0f0fb6a862c31506ebde0542d92e35912beabac
Keshav-Code-er <keshavinsc446@gmail.com>
2025-06-07 22:05:29 +0530

    ansible_role_app_version

--- terraform/catalogue.sh
+++ terraform/catalogue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-App_version=$1
-echo "app version: $App_version"
+app_version=$1
+echo "app version: $app_version"
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
 cd /tmp
-ansible-pull -U https://github.com/Keshav-Code-er/Ansible-roboshop-roles-tf.git -e component=catalogue -e app_version=$App_version main.yaml
+ansible-pull -U https://github.com/Keshav-Code-er/Ansible-roboshop-roles-tf.git -e component=catalogue -e app_version=$app_version main.yaml
