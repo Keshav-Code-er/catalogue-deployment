bbd6810f02d3e1a13006577e3dcba6c6d717d27e
Keshav-Code-er <keshavinsc446@gmail.com>
2025-06-19 15:48:51 +0530

    aoo_version

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,6 +74,50 @@ resource "null_resource" "delete_instance" {
   
 }
 
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${var.project-name}-${var.common_tags.component}-${var.env}"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.aws_ssm_parameter.vpc_id.value
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2 #consider as healthy checks are success
+    interval            = 15
+    matcher             = "200-299"
+    path                = "/health"
+    port                = 8080
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 3
+
+  }
+
+}
+
+resource "aws_launch_template" "catalogue" {
+  name = "${var.project-name}-${var.common_tags.component}-${var.env}"
+
+  # Here AMI ID should be the one we just created
+  image_id = aws_ami_from_instance.catalogue_ami.id
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "t2.micro"
+
+
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "catalogue"
+    }
+  }
+
+  user_data = filebase64("${path.module}/catalogue.sh")
+}
+
+
 output "app_version" {
   value = var.app_version
 
